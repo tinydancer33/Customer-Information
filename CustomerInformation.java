a5c3b936fb7c72837b564edd4d65a9828a50f09e
tinydancer33 <55039501+tinydancer33@users.noreply.github.com>
2019-09-14 15:28:54 -0600

    Add files via upload

---
+++ CustomerInformation.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package customerinformation;
+
+import java.util.Scanner;
+
+/*Name: Erica Osborn
+Date: 09/14/19
+Description: Customer information database application.
+*/
+public class CustomerInformation {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Customer Viewer: Version 1.0 ");
+        System.out.println();
+        
+        Scanner sc = new Scanner(System.in); 
+       
+    }
+    
+}
